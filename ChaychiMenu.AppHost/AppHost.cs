5de522d484650db272913b7292c61b25df36dbfe
theahmadzada <ahmad.ahmadzada999@gmail.com>
2026-09-11 11:20:47 +0400

    feat: add env configuration

--- ChaychiMenu.AppHost/AppHost.cs
+++ ChaychiMenu.AppHost/AppHost.cs
@@ -15,6 +15,11 @@ var migrations = builder.AddProject<Projects.ChaychiMenu_MigrationService>("migr
 var minio = builder.AddMinioContainer("minio")
     .WithDataVolume();
 
+var jwtIssuer = builder.AddParameter("jwt-issuer");
+var jwtAudience = builder.AddParameter("jwt-audience");
+var jwtSigningKey = builder.AddParameter("jwt-signing-key", secret: true);
+var refreshTokenKey = builder.AddParameter("refresh-token-key", secret: true);
+
 builder.AddProject<Projects.ChaychiMenu_WebApi>("webapi")
     .WithExternalHttpEndpoints()
     .WithReference(psql)
@@ -24,6 +29,10 @@ builder.AddProject<Projects.ChaychiMenu_WebApi>("webapi")
     .WithReference(redis)
     .WaitFor(redis)
     .WithReference(minio)
-    .WaitFor(minio);
+    .WaitFor(minio)
+    .WithEnvironment("JwtSettings_Issuer", jwtIssuer)
+    .WithEnvironment("JwtSettings_Audience", jwtAudience)
+    .WithEnvironment("JwtSettings_SigningKey", jwtSigningKey)
+    .WithEnvironment("RefreshTokenSettings_Key", refreshTokenKey);
 
 builder.Build().Run();
